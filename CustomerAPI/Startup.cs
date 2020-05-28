092fd5a38c19ad807f8b5bd8fb8ffed1ba1f4a75
Vivek Kosare <v_kosare@yahoo.com>
2020-05-28 15:13:29 +0200

    Implemented API documentation using Swagger/Open API

--- CustomerAPI/Startup.cs
+++ CustomerAPI/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
 
 namespace CustomerAPI
@@ -30,6 +31,11 @@ namespace CustomerAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Customer API", Version = "v1" });
+            });
             services.AddControllers(action =>
             {
                 action.ReturnHttpNotAcceptable = true;
@@ -78,6 +84,16 @@ namespace CustomerAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
